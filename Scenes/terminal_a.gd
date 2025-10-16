42e7cb68171412bdb49fb2de445762fc341ecfab
Xrime <felixadesola04@gmail.com>
2025-10-16 13:13:18 +0100

    Improving the two signal terminal

---
+++ Scenes/terminal_a.gd
@@ -0,0 +1,16 @@
+extends Area3D
+
+@export var terminal_name := "A" # Change to "B" for the second one
+var player_near := false
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+	connect("body_exited", Callable(self, "_on_body_exited"))
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		player_near = true
+
+func _on_body_exited(body):
+	if body.name == "Player":
+		player_near = false
